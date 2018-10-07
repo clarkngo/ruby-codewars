00901f0468bfa2f0aabf02ae56788ffe8c17b29d
clarkngo <clarkngo@gmail.com>
2018-10-06 22:48:01 -0700

    function square a number

---
+++ 5th_problem_set/function_2_squaring_an_argument.rb
@@ -0,0 +1,9 @@
+=begin
+Now you have to write a function called square 
+that takes an argument and returns the square of it.
+=end
+
+# Write the "square"-function here
+def square(num)
+  return num * num
+end
