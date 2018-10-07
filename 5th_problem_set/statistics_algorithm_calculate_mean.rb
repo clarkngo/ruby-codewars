fc6681eabcb88bbff73d74e09f5438068dc66b68
clarkngo <clarkngo@gmail.com>
2018-10-06 23:25:43 -0700

    calculate mean of an array

---
+++ 5th_problem_set/statistics_algorithm_calculate_mean.rb
@@ -0,0 +1,31 @@
+=begin
+
+Create a mathematical algorithm to calculate the mean of a series of numbers. 
+The mean of a series of numbers is the sum of all items in a set, 
+divided by the number of items in that set.
+
+Examples: 
+calc_mean([]) should return 0 
+calc_mean([1, 2, 3]) should return 2.0 
+calc_mean([6, 8, 10]) should return 8.0 
+calc_mean([15, 30, 60, 120, 240]) should return 93.0
+
+Inputs are always populated (non-empty) arrays. 
+Input arrays never include non-integer or non-float elements. 
+Non-array inputs return 0.
+
+=end
+
+def calc_mean(ary)
+  sum = 0
+  mean = 0
+  if !ary.is_a?(Array)
+    0
+  elsif ary.empty?
+    0
+  else
+    # Your code goes here 
+    sum = ary.reduce(0, :+)
+    return mean = sum / ary.length
+  end
+end
