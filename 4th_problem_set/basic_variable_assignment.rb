f0c905c62a5d8c20a60b9f38f824f557ce758f87
clarkngo <clarkngo@gmail.com>
2018-10-06 22:26:50 -0700

    assignment operator

---
+++ 4th_problem_set/basic_variable_assignment.rb
@@ -0,0 +1,12 @@
+=begin
+
+This code should store "codewa.rs" as a 
+variable called "name" but it's not working. Can you figure out why?
+
+=end
+
+# answer: change == to =
+
+a = "code"
+b = "wa.rs"
+name = a + b
