7d8ec4c0b6ad3b3c2701e2d69d50fc402af9e20b
clarkngo <clarkngo@gmail.com>
2018-10-06 21:39:54 -0700

    array to string

---
+++ 2nd_problem_set/sentence_smash.rb
@@ -0,0 +1,28 @@
+=begin
+
+Sentence Smash
+Write a method smash that takes an array of words and smashes 
+them together into a sentence and returns the sentence. You 
+can ignore any need to sanitize words or add punctuation, but 
+you should add spaces between each word. Be careful, there 
+shouldn't be a space at the beginning or the end of the sentence!
+
+Example
+words = ['hello', 'world', 'this', 'is', 'great']
+smash(words) # returns "hello world this is great"
+Assumptions
+You can assume that you are only given words.
+You cannot assume the size of the array.
+You can assume that you will always get an array.
+What We're Testing
+We're testing basic loops and string manipulation. This is for 
+beginners who are just learning loops and string manipulation.
+
+=end
+
+# Smash Words
+def smash(words)
+  return words.join(" ")
+end
+
+smash(["hello", "world"])
