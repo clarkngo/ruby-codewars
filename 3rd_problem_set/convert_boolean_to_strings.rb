dfab56cbe13ea81574252f6c4b8f03316bb16066
clarkngo <clarkngo@gmail.com>
2018-10-06 22:09:32 -0700

    bool to string

---
+++ 3rd_problem_set/convert_boolean_to_strings.rb
@@ -0,0 +1,14 @@
+=begin
+
+Complete the method that takes a boolean value and 
+return a "Yes" string for true, or a "No" string for false.
+
+=end
+
+def bool_to_word bool
+  if bool == true
+    return "Yes"
+  else
+    return "No"
+  end
+end
