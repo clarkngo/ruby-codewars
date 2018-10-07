dcb5c5a82a5972c51afb604b7714dc9507299e24
clarkngo <clarkngo@gmail.com>
2018-10-06 20:35:52 -0700

    remove duplicates in array

--- 2nd_problem_set/remove_duplicates.rb
+++ 2nd_problem_set/remove_duplicates.rb
@@ -9,6 +9,15 @@ array then it should also be that 52 appears
 before 10 in the returned array.
 =end
 
+# Define the unique method that removes duplicates
 def unique(integers)
-
+  filteredIntegers = []
+  for i in 0..integers.length - 1
+    if !filteredIntegers.include? integers[i]
+      filteredIntegers.push(integers[i])
+    end
+  end
+  return filteredIntegers
 end
+
+unique([1, 2, 3, 3, 2, 1, 2, 3, 1, 1, 3, 2])
