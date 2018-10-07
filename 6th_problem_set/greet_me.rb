8c1685a6ad0f284acdb4c059fab4f959dc24969f
clarkngo <clarkngo@gmail.com>
2018-10-07 16:49:22 -0700

    capitalize first letter

---
+++ 6th_problem_set/greet_me.rb
@@ -0,0 +1,14 @@
+=begin
+
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+
+greet("maya") # returns "Hello Maya!"
+greet("JACK") # returns "Hello Jack!"
+
+=end
+
+def greet(name)
+  return "Hello " + name.capitalize + "!"
+end
