5b8b168c4cb8f140611530a755d06d65858c6580
clarkngo <clarkngo@gmail.com>
2018-10-06 22:49:58 -0700

    function multiply two numbers

---
+++ 5th_problem_set/function_3_multiplying_two_nums.rb
@@ -0,0 +1,10 @@
+=begin
+This function has to be called multiply and needs to 
+take two numbers as arguments, and has to return the 
+multiplication of the two arguments.
+=end
+
+# Write here your "multiply"-function
+def multiply(num1, num2)
+  return num1 * num2
+end
