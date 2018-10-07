22ecbcba1d394e69b1cab5411a65c5612df2674e
clarkngo <clarkngo@gmail.com>
2018-10-06 20:52:00 -0700

    get random color when instantiated

---
+++ 2nd_problem_set/color_ghost.rb
@@ -0,0 +1,16 @@
+=begin
+Color Ghost
+Create a class Ghost
+
+Ghost objects are instantiated without any arguments.
+
+Ghost objects are given a random color attribute of 
+white" or "yellow" or "purple" or "red" when instantiated
+=end
+
+class Ghost
+  attr_accessor :color
+  def initialize
+    @color = ["white", "yellow", "purple", "red"].sample
+  end
+end
